793fcdf10df36e08d902a856e07c66ae495371ef
KhairiAbidi <abidikhairi32@gmail.com>
2019-05-23 01:32:33 +0100

    hadoop conf

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+HADOOP_VERSION=3.2.0
+HADOOP=hadoop-$HADOOP_VERSION
+HADOOP_URL=https://archive.apache.org/dist/hadoop/core/$HADOOP/$HADOOP.tar.gz
+HADOOP_INSTALL_DIR=$HOME/opt/
+HADOOP_CONF_TEMPLATE=$(pwd)/etc/hadoop
+
+
+wget $HADOOP_URL --progress=bar:mega

c8b304134cd30732fae6cd502b8dc4db405de387
KhairiAbidi <abidikhairi32@gmail.com>
2019-06-06 13:24:05 +0100

    mv /home/khayri/opt to /home/khayri/apache

--- spark/install-spark.sh
+++ spark/install-spark.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 
-SPARK_URL=https://www-us.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
-SPARK_ARCHIVE=spark-2.4.0-bin-hadoop2.7.tgz
-SPARK_INSTALL_DIR=/opt
+SPARK_URL=https://archive.apache.org/dist/spark/spark-2.4.3/spark-2.4.3-bin-without-hadoop.tgz
+SPARK_ARCHIVE=spark-2.4.3-bin-without-hadoop.tgz
+SPARK_INSTALL_DIR=$HOME/apache
 TMP_DIR=/tmp
 
-if [[ $UID -ne 0 ]]; then
-  echo -en '\E[45;32m' "\033[1m MUST BE ROOT (EACCESS) \033[0m \n"   # Green
-  tput sgr0
-  exit 13
-fi
 
 echo -en '\E[37;32m' "\033[1m *************** SWITCH TO /tmp *************** \033[0m \n"
-tput sgr0
 pushd $TMP_DIR
 wget --show-progress $SPARK_URL
 
-echo -en '\E[37;32m' "\033[1m *************** EXTRACT TO /opt *************** \033[0m \n"
-tput sgr0
+echo -en '\E[37;32m' "\033[1m *************** EXTRACT TO $HOME/apache *************** \033[0m \n"
 tar zxvf $SPARK_ARCHIVE -C $SPARK_INSTALL_DIR
